48cd6b6d6ddaba6aa9f1da7e0831620140da5445
jeffnatividad <jefferson.natividad@gmail.com>
2022-07-26 13:47:30 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,4 +47,4 @@ streamlit.write("Thanks for adding",add_my_fruit)
 
 
 
-mycur.execute("insert into fruit_load_list values('from streamlit')")
+my_cur.execute("insert into fruit_load_list values('from streamlit')")
